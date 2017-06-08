53f285cee320d833999bc258fd76210c6283b6ad
yesalam <itisalam@gmail.com>
2017-06-08 18:36:40 +0530

    Added CardView for Televerification fragment

---
+++ app/src/main/java/io/github/yesalam/acquaint/TeleVeriRecyclerAdapter.java
@@ -0,0 +1,35 @@
+package io.github.yesalam.acquaint;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yesalam on 08-06-2017.
+ */
+
+public class TeleVeriRecyclerAdapter extends RecyclerView.Adapter<TeleVeriRecyclerAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_televerification,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
