ea3ef0ba7ba64de5236eea607640226e70aaa974
yesalam <itisalam@gmail.com>
2017-06-09 03:47:42 +0530

    Fragment for indi case activity created

--- app/src/main/java/io/github/yesalam/acquaint/Fragments/CaseBasicDetail.java
+++ app/src/main/java/io/github/yesalam/acquaint/Fragments/CaseBasicDetail.java
@@ -1,10 +1,24 @@
 package io.github.yesalam.acquaint.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.yesalam.acquaint.R;
 
 /**
  * Created by yesalam on 08-06-2017.
  */
 
 public class CaseBasicDetail extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_case_basic_detail,container,false);
+        return view;
+    }
 }
--- app/src/main/java/io/github/yesalam/acquaint/Fragments/CaseGuarantor.java
+++ app/src/main/java/io/github/yesalam/acquaint/Fragments/CaseGuarantor.java
@@ -1,10 +1,23 @@
 package io.github.yesalam.acquaint.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.yesalam.acquaint.R;
 
 /**
  * Created by yesalam on 09-06-2017.
  */
 
 public class CaseGuarantor extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_case_guarantor,container,false);
+        return view;
+    }
 }
